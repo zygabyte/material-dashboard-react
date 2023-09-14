16c0a105486d9660ba52ebd7737ced446b118b6f
Eseosa Ehioghae <eseosa.ehioghae@verisk.com>
2023-09-14 16:08:12 +0100

    implement mock assignments table

---
+++ src/layouts/billing/data/assignmentsTableData.js
@@ -0,0 +1,177 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDBadge from "components/MDBadge";
+
+// Images
+import team2 from "assets/images/team-2.jpg";
+import team3 from "assets/images/team-3.jpg";
+import team4 from "assets/images/team-4.jpg";
+
+export default function data() {
+  const Assignment = ({ image, name, email }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+        <MDTypography variant="caption">{email}</MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const Job = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  return {
+    columns: [
+      { Header: "insured", accessor: "insured", width: "30%", align: "left" },
+      { Header: "claim number", accessor: "claim", align: "left" },
+      { Header: "assignment status", accessor: "status", align: "center" },
+      { Header: "data set", accessor: "data", align: "left" },
+      { Header: "estimate", accessor: "estimate", align: "center" },
+    ],
+
+    rows: [
+      {
+        insured: <Assignment image="" name="Mr. John Michael" email="johnmichael@gmail.com" />,
+        claim: <Job title="H02929109LL3901" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="inspection" color="blue" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Desk
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £3,500
+          </MDTypography>
+        ),
+      },
+      {
+        insured: <Assignment name="Mr. James White" email="jameswhite@gmail.com" />,
+        claim: <Job title="H02134709LL3901" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="canceled" color="dark" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Loss Adjuster
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £2,000
+          </MDTypography>
+        ),
+      },
+      {
+        insured: <Assignment name="Mrs. Laura Smith" email="laurasmith@gmail.com" />,
+        claim: <Job title="H01224709LL3901" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="fulfillment" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Desk
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £3,000
+          </MDTypography>
+        ),
+      },
+      {
+        insured: <Assignment name="Dr. Jane Mark" email="janemark@gmail.com" />,
+        claim: <Job title="H01224709SL3322" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="fulfillment" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Repair Contractor
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £3,500
+          </MDTypography>
+        ),
+      },
+      {
+        insured: <Assignment name="Mr. Johnny Cash" email="johnnycash@gmail.com" />,
+        claim: <Job title="H01224779SA3431" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="fulfillment" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Desk
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £7,400
+          </MDTypography>
+        ),
+      },
+      {
+        insured: <Assignment name="Mr. Johnny Cash" email="johnnycash@gmail.com" />,
+        claim: <Job title="H01224779SA3431" description="" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="fulfillment" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        data: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            Callisto - Desk
+          </MDTypography>
+        ),
+        estimate: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            £7,400
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
--- src/layouts/billing/index.js
+++ src/layouts/billing/index.js
@@ -18,68 +18,55 @@ import Grid from "@mui/material/Grid";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
 
 // Material Dashboard 2 React examples
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
-import MasterCard from "examples/Cards/MasterCard";
-import DefaultInfoCard from "examples/Cards/InfoCards/DefaultInfoCard";
 
 // Billing page components
-import PaymentMethod from "layouts/billing/components/PaymentMethod";
-import Invoices from "layouts/billing/components/Invoices";
-import BillingInformation from "layouts/billing/components/BillingInformation";
-import Transactions from "layouts/billing/components/Transactions";
+import Card from "@mui/material/Card";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import assignmentTableData from "layouts/billing/data/assignmentsTableData";
 
 function Billing() {
+  const { columns, rows } = assignmentTableData();
   return (
     <DashboardLayout>
-      <DashboardNavbar absolute isMini />
-      <MDBox mt={8}>
-        <MDBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={8}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} xl={6}>
-                  <MasterCard number={4562112245947852} holder="jack peterson" expires="11/22" />
-                </Grid>
-                <Grid item xs={12} md={6} xl={3}>
-                  <DefaultInfoCard
-                    icon="account_balance"
-                    title="salary"
-                    description="Belong Interactive"
-                    value="+$2000"
-                  />
-                </Grid>
-                <Grid item xs={12} md={6} xl={3}>
-                  <DefaultInfoCard
-                    icon="paypal"
-                    title="paypal"
-                    description="Freelance Payment"
-                    value="$455.00"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <PaymentMethod />
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} lg={4}>
-              <Invoices />
-            </Grid>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Assignments
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
           </Grid>
-        </MDBox>
-        <MDBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={7}>
-              <BillingInformation />
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <Transactions />
-            </Grid>
-          </Grid>
-        </MDBox>
+        </Grid>
       </MDBox>
       <Footer />
     </DashboardLayout>
