0282f05695613ca276005a441a7c1bd9ea1209ca
Eseosa Ehioghae <eseosa.ehioghae@verisk.com>
2023-10-13 13:15:22 +0100

    add claimNumber to FilterClaimRequest

--- src/core/models/claims/requests/index.ts
+++ src/core/models/claims/requests/index.ts
@@ -1,6 +1,7 @@
 import {PaginatedRequest} from "../../common/requests";
 
 export interface FilterClaimRequest extends PaginatedRequest {
+    claimNumber?: string | null;
     customerName?: string | null;
     claimStatus?: number | null;
     claimType?: number | null;
