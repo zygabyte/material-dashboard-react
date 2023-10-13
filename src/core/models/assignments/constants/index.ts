5d8fec1e62e208ecc3005a0503a634643b509559
Eseosa Ehioghae <eseosa.ehioghae@verisk.com>
2023-10-13 13:14:51 +0100

    add assignments constants and models

---
+++ src/core/models/assignments/constants/index.ts
@@ -0,0 +1,17 @@
+
+//Assignment Statuses
+export const Pending = 0;
+export const AssignmentStatuses = [
+  { value: Pending, name: 'Pending' },
+];
+
+
+//Job Types
+export const General = 0;
+export const Estimation = 1;
+export const Validation = 2;
+export const JobTypes = [
+  { value: General, name: 'General' },
+  { value: Estimation, name: 'Estimation' },
+  { value: Validation, name: 'Validation' },
+];
---
+++ src/core/models/assignments/index.ts
@@ -0,0 +1,13 @@
+export interface AssignmentModel {
+  assignmentId: string;
+  claimId: string;
+  claimNumber: string;
+  customerName: string;
+  customerEmail: string;
+  dataSet: string;
+  assignee: string;
+  assignmentStatus: number;
+  jobType: number;
+  estimate: number;
+  notes: string;
+}
